143b0085f4edcf9224eee3ae186aeb53a3a71827
kce3kor <kamal.shrestha@in.bosch.com>
2023-08-28 14:00:53 +0530

    matplotlib integration

---
+++ streamlit-wiki/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title("Simulation[tm]")
+st.write("One demo simulation")
+
+
+x = st.slider("Slope", min_value = 0.01, max_value = 0.1, step = 0.01)
+y = st.slider("Noise", min_value = 0.01, max_value = 0.1, step = 0.01)
+
+
+st.write(f"x={x},  y={y}")
+
+values = np.cumprod(1+np.random.normal(x,y, (100, 10)), axis=0)
+
+for i in range(values.shape[1]):
+    plt.plot(values[:, i])
+
+st.pyplot()
